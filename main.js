f0752691bdd49285fd425c60fc555e2456213dc4
JStameus <joseph.stameus@gmail.com>
2021-04-29 10:18:55 +0200

    Changed main function so that it waits for events to be fetched from the API before populating the calendar

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
+const BASEURL = "http://localhost:3000/api";
+var apiQuery = "u=jstameus";
+var eventList = [];
+
 function main() {
     // TODO: Events should be fetched from the API before calendar initialization
-    initCalendar();
+    fetch(`${BASEURL}/${apiQuery}`)
+        .then(response => response.json())
+        .then(data => {
+            // TODO: I am getting an array of data here. Is there a problem in
+            // the client or the API?
+            data[0].events.forEach(obj => eventList.push(obj));
+            initCalendar();
+        });
 }
 
 // APP ENTRY POINT
